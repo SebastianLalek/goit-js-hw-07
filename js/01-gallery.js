b88e927e57d67c47d1d42894e2aa5daeabce9646
Sebastian Lalek <123250026+SebastianLalek@users.noreply.github.com>
2023-08-19 16:50:41 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,31 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
 
 galleryItems.forEach(addItem);
 
 function addItem(element) {
-const galleryItem = document.createElement("li")
-galleryItem.classList.add("gallery__item")
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
 
-const galleryLink = document.createElement("a")
-galleryLink.classList.add("gallery__link")
-galleryLink.href = element.original;
-galleryLink.onclick = () => {
-    basicLightbox.create(`<img width="800" src=${element.original}>`).show()
-}
-galleryLink.addEventListener("click", e => e.preventDefault());
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = element.original;
+  galleryLink.onclick = () => {
+    basicLightbox.create(`<img width="800" src=${element.original}>`).show();
+  };
+  galleryLink.addEventListener("click", (e) => e.preventDefault());
 
-const galleryImage = document.createElement("img")
-galleryImage.classList.add("gallery__image")
-galleryImage.src = element.preview;
-galleryImage.dataset.source = element.original;
-galleryImage.alt = element.description;
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = element.preview;
+  galleryImage.dataset.source = element.original;
+  galleryImage.alt = element.description;
 
-galleryLink.append(galleryImage);
-galleryItem.append(galleryLink);
-gallery.append(galleryItem)
+  galleryLink.append(galleryImage);
+  galleryItem.append(galleryLink);
+  gallery.append(galleryItem);
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach(addItem);
+
+function addItem(element) {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = element.original;
+  galleryLink.addEventListener("click", (e) => e.preventDefault());
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = element.preview;
+  galleryImage.alt = element.description;
+
+  galleryLink.append(galleryImage);
+  galleryItem.append(galleryLink);
+  gallery.append(galleryItem);
+}
+
+gallery.addEventListener("click", () => {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+});
